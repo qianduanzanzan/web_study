2bc14beab9673363680dfef3eed9f300efa55eb2
zdm <1248965034@qq.com>
2022-04-06 14:58:15 +0800

    js继承

---
+++ js/继承/借用构造函数继承.js
@@ -0,0 +1,19 @@
+/**
+ * 借用构造函数继承
+ */
+
+function superType(){
+    this.friends = ['小明','小红']
+    this.sayHi = function(){
+        console.log('hi')
+    }
+}
+
+function subType(){
+    superType.call(this)
+}
+
+var subInstance = new subType()
+
+console.log(subInstance.friends)
+subInstance.sayHi()
---
+++ js/继承/原型链继承.js
@@ -0,0 +1,30 @@
+/**
+ * 原型链继承
+ */
+
+function superType(){
+    this.friends = ['小明','小红']
+    this.sayHi = function(){
+        console.log('hi')
+    }
+}
+
+function subType(){}
+
+subType.prototype = new superType()
+
+var subInstance1 = new subType()
+
+console.log(subInstance1.friends)
+
+subInstance1.sayHi()
+
+/**
+ * 缺点：对父类的引用类型的操作会修改掉原型链上的值
+ */
+
+subInstance1.friends.push('小王')
+
+var subInstance2 = new subType()
+
+console.log(subInstance2.friends)
