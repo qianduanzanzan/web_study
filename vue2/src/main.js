e1177b2c8de1fcda0519ae4bf7ee712651981fab
zdm <1248965034@qq.com>
2022-08-10 17:57:13 +0800

    111111

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "./vue-2.6.14/src/core/index";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => {
+    console.log(h);
+    return h(App);
+  },
+}).$mount("#app");
